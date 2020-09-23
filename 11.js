293a9a10d1a35917f8862019efc75726707b9fac
ElenaKulikova80 <71148521+ElenaKulikova80@users.noreply.github.com>
2020-09-23 23:09:11 +0300

    Add files via upload

---
+++ 11.js
@@ -0,0 +1,20 @@
+﻿x=prompt("Введите число:");
+n=prompt("Введите степень:");
+const expon =(x,n) => {
+   if (n!=1){
+    if (n>0){
+      return x=expon(x,n-1)*x;
+    } else if(n<0){
+      return 1.0 / expon(x,-n);
+    } else if (n===0){
+      return 1
+    } 
+  } else{
+    return x;
+  }
+}
+if (isNaN(+x)||isNaN(+n)){
+  alert("Введите числа")
+}else {
+  console.log(expon(+x,+n))
+}
